4d48aa87afbe0f93aa42422c8e4062e80dee6bc9
Sujal Adhikari <sadhikari4@caldwell.edu>
2024-10-16 12:11:50 -0400

    First commit

---
+++ index.py
@@ -0,0 +1,6 @@
+# init ##creating git repositry and then make your file include in the git
+# touch ##create a new file in the git 
+# add ##Add files on the git 
+def greet():
+    return "Hello world!"
+print(greet())
